bb40c96a5a5b89af7b2ade3838f12d3da8ffeb59
UchihaMarkoo <marcocarrasco212@gmail.com>
2026-05-15 19:19:19 -0400

    arreglo de serviceimpl msexamenes

--- msexamenes/src/main/java/com/clinic/msexamenes/service/ExamenServiceImpl.java
+++ msexamenes/src/main/java/com/clinic/msexamenes/service/ExamenServiceImpl.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Service;
 
 import com.clinic.msexamenes.client.PacienteClient;
 import com.clinic.msexamenes.dto.*;
-
+import com.clinic.msexamenes.exception.ExamenNotFoundException;
+import com.clinic.msexamenes.mapper.ExamenMapper;
+import com.clinic.msexamenes.model.Examen;
 import com.clinic.msexamenes.repository.ExamenRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,39 +17,106 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class ExamenServiceImpl implements ExamenService {
+public class ExamenServiceImpl
+        implements ExamenService {
 
     private final ExamenRepository repository;
 
     private final PacienteClient pacienteClient;
 
     @Override
-    public ExamenResponseDTO crear(ExamenRequestDTO dto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'crear'");
+    public ExamenResponseDTO crear(
+            ExamenRequestDTO dto) {
+
+        log.info(
+                "Creando examen paciente {}",
+                dto.getPacienteId());
+
+        pacienteClient.buscarPaciente(
+                dto.getPacienteId());
+
+        Examen examen = ExamenMapper.toEntity(dto);
+
+        Examen guardado = repository.save(examen);
+
+        log.info(
+                "Examen creado ID {}",
+                guardado.getId());
+
+        return ExamenMapper.toDTO(
+                guardado);
+
     }
 
     @Override
     public List<ExamenResponseDTO> listar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listar'");
+
+        log.info(
+                "Listando exámenes");
+
+        return repository.findAll()
+                .stream()
+                .map(ExamenMapper::toDTO)
+                .toList();
+
     }
 
     @Override
     public ExamenResponseDTO buscarPorId(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buscarPorId'");
+
+        log.info(
+                "Buscando examen {}", id);
+
+        Examen examen = repository.findById(id)
+                .orElseThrow(() -> new ExamenNotFoundException());
+
+        return ExamenMapper.toDTO(
+                examen);
+
     }
 
     @Override
-    public ExamenResponseDTO actualizar(Long id, ExamenRequestDTO dto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'actualizar'");
+    public ExamenResponseDTO actualizar(
+            Long id,
+            ExamenRequestDTO dto) {
+
+        log.info(
+                "Actualizando examen {}", id);
+
+        Examen examen = repository.findById(id)
+                .orElseThrow(() -> new ExamenNotFoundException());
+
+        examen.setPacienteId(
+                dto.getPacienteId());
+
+        examen.setTipoExamen(
+                dto.getTipoExamen());
+
+        examen.setFecha(
+                dto.getFecha());
+
+        examen.setResultado(
+                dto.getResultado());
+
+        examen.setEstado(
+                dto.getEstado());
+
+        return ExamenMapper.toDTO(
+                repository.save(examen));
+
     }
 
     @Override
     public void eliminar(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'eliminar'");
+
+        log.info(
+                "Eliminando examen {}", id);
+
+        Examen examen = repository.findById(id)
+                .orElseThrow(() -> new ExamenNotFoundException());
+
+        repository.delete(examen);
+
     }
+
 }
