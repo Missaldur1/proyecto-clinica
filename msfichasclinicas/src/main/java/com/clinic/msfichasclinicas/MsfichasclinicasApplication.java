a62efcc368014baf06dda60f9ce02d07d7bc8a96
UchihaMarkoo <marcocarrasco212@gmail.com>
2026-05-15 19:42:13 -0400

    añade msfichasclinicas y arreglo yaml msexamenes

---
+++ msfichasclinicas/src/main/java/com/clinic/msfichasclinicas/MsfichasclinicasApplication.java
@@ -0,0 +1,13 @@
+package com.clinic.msfichasclinicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsfichasclinicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsfichasclinicasApplication.class, args);
+	}
+
+}
